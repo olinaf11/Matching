018deb3bb9a83608522ccedde23c764da35f2052
Tendrynyavo <94636671+Tendrynyavo@users.noreply.github.com>
2022-11-19 06:35:42 +0300

    Update BddObject.java

--- WEB-INF/classes/connection/BddObject.java
+++ WEB-INF/classes/connection/BddObject.java
@@ -112,7 +112,6 @@ public abstract class BddObject {
 
     public Object[] convertToObject(ResultSet result, int attribut) throws Exception {
         Field[] attributs = this.getClass().getDeclaredFields();
-        Vector<Object> objects = new Vector<Object>();
         while (result.next()) {
             Object object = this.getClass().getConstructor().newInstance();
             for (int i = 0; i < attribut; i++) {
