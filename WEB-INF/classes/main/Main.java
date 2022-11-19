708ec2f342ae59d57530f7fe3852416205ca661b
Tendrynyavo <Tendrirazafimiandrisoa@gmail.com>
2022-11-19 09:33:08 +0300

    main commit

--- WEB-INF/classes/main/Main.java
+++ WEB-INF/classes/main/Main.java
@@ -1,13 +1,7 @@
 package main;
 
-import java.sql.Date;
-import match.Match;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        Match match = new Match();
-        match.setIdUser("USR0003");
-        match.setUser();
-        System.out.println(match.getUser().getNom());
+        
     }
 }
